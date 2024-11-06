47b10207eec365715918bab8ac284a6c8218c7b1
Gabriely Carrari <gabymcarrari@gmail.com>
2024-11-06 17:04:54 -0300

    ✨ add login form

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,49 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import api from "./axiosApi";
+import LoginForm from "./LoginForm";
+import FormButtons from './FormButtons';
+import Loading from './Loading';
+import handleChange from './handleChange';
+import parseErrors from './parseErrors';
+import { login } from './authService';
+
 const Home = () => {
+    const [inputs, setInputs] = useState({});
+    const [errors, setErrors] = useState({});
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        setLoading(true);
+        if (login(inputs.email, inputs.senha)) {
+            navigate('/orders');
+        } else {
+            setErrors({ email: "E-mail ou senha inválidos" });
+        }
+        setLoading(false);
+    }
+
+    function localHandleChange(event) {
+        handleChange(event, inputs, setInputs);
+    }
+
     return (
-        <h1>Página Principal</h1>
+        <>
+            <h1 className='mt-2'>Página Principal</h1>
+            <hr />
+            <h2>Entrar</h2>
+            <form onSubmit={handleSubmit} noValidate autoComplete="off">
+                <LoginForm handleChange={localHandleChange} errors={errors} inputs={inputs} />
+                <div>
+                    <button type="submit" className="btn btn-primary">
+                        Entrar
+                    </button>
+                </div>
+            </form>
+            {loading && <Loading />}
+        </>
     )
 }
 
---
+++ src/LoginForm.jsx
@@ -0,0 +1,19 @@
+import FormInput from "./FormInput";
+
+const LoginForm = ({ handleChange, inputs, errors }) => {
+    return (
+        <>
+            <div className="row flex-column">
+                <div className="col-6 mb-3">
+                    <FormInput type="email" field="email" label="E-mail" onChange={handleChange} error={errors?.email} autofocus={true} value={inputs?.email} />
+                </div>
+                <div className="col-6 mb-3">
+                    <FormInput type="password" field="senha" label="Senha" onChange={handleChange} error={errors?.email} value={inputs?.senha} />
+                </div>
+            </div>
+        </>
+    );
+
+}
+
+export default LoginForm;
